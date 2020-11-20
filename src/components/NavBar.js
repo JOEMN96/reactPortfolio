eb38358d3b34dc6f669ac57f264dfddc96601e01
Joemn <Aruljoe37@gmail.com>
2020-11-21 00:12:31 +0530

    navbar Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,39 +2,45 @@ import React, { useState } from "react";
 
 function NavBar() {
   const [isToggled, setisToggled] = useState(false);
-
+  window.addEventListener("scroll", () => {
+    let nav = document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0);
+  });
   return (
-    <nav>
-      <div className="navWrapper">
-        <div className="navMain">
-          <div className="logo">
-            <h2>JoeMN</h2>
-          </div>
+    <>
+      <nav>
+        <div className="navWrapper">
+          <div className="navMain">
+            <div className="logo">
+              <h2>JoeMN</h2>
+            </div>
 
-          <div className="navTriggerWrapper">
-            <div
-              className="trigger"
-              onClick={() => {
-                setisToggled(!isToggled);
-              }}
-            >
-              <span></span>
-              <span></span>
-              <span></span>
-              <div></div>
+            <div className="navTriggerWrapper">
+              <div
+                className="trigger"
+                onClick={() => {
+                  setisToggled(!isToggled);
+                }}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+                <div></div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className={`navLinks ${isToggled ? "nav-show " : "nav-hide "}`}>
-          <a className="animDelayFIx" href="">
-            About
-          </a>
-          <a href="">Works</a>
-          <a href="">Contact</a>
+          <div className={`navLinks ${isToggled ? "nav-show " : "nav-hide "}`}>
+            <a className="animDelayFIx" href="">
+              About
+            </a>
+            <a href="">Works</a>
+            <a href="">Contact</a>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <div style={{ height: "200vh" }} className="col"></div>
+    </>
   );
 }
 
