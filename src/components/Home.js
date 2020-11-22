9bc0b7ce13388f5fc67c97f943b9a83ee28a4c9a
Joemn <Aruljoe37@gmail.com>
2020-11-22 22:22:49 +0530

    scrolling text added in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,74 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Particles from "react-particles-js";
 import particlesConfig from "../assets/ParticlesConfig";
 function Home() {
+  useEffect(() => {
+    // function([string1, string2],target id,[color1,color2])
+    consoleText(
+      ["Hello World.", "Iam Joe Mon", "A web developer & designer."],
+      "text",
+      ["white", "white", "white"]
+    );
+
+    function consoleText(words, id, colors) {
+      if (colors === undefined) colors = ["#fff"];
+      var visible = true;
+      var con = document.getElementById("console");
+      var letterCount = 1;
+      var x = 1;
+      var waiting = false;
+      var target = document.getElementById(id);
+      target.setAttribute("style", "color:" + colors[0]);
+      window.setInterval(function () {
+        if (letterCount === 0 && waiting === false) {
+          waiting = true;
+          target.innerHTML = words[0].substring(0, letterCount);
+          window.setTimeout(function () {
+            var usedColor = colors.shift();
+            colors.push(usedColor);
+            var usedWord = words.shift();
+            words.push(usedWord);
+            x = 1;
+            target.setAttribute("style", "color:" + colors[0]);
+            letterCount += x;
+            waiting = false;
+          }, 1000);
+        } else if (letterCount === words[0].length + 1 && waiting === false) {
+          waiting = true;
+          window.setTimeout(function () {
+            x = -1;
+            letterCount += x;
+            waiting = false;
+          }, 1000);
+        } else if (waiting === false) {
+          target.innerHTML = words[0].substring(0, letterCount);
+          letterCount += x;
+        }
+      }, 120);
+      window.setInterval(function () {
+        if (visible === true) {
+          con.className = "console-underscore hidden";
+          visible = false;
+        } else {
+          con.className = "console-underscore";
+
+          visible = true;
+        }
+      }, 400);
+    }
+  }, []);
+
   return (
     <div style={{ overflow: "hidden" }}>
       <Particles params={particlesConfig} />
-      <h1 className="floatTxt">Hello</h1>
+
+      {/* <h1 className="floatTxt">Hello</h1> */}
+      <h3 className="floatTxt">
+        <span id="text"></span>{" "}
+        <span className="console-underscore" id="console">
+          &#95;
+        </span>
+      </h3>
     </div>
   );
 }
