c234914b068efe4a9e365fc4ad1141fea63f357d
Joemn <Aruljoe37@gmail.com>
2020-11-22 16:59:03 +0530

    particles added to the home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Particles from "react-particles-js";
+import particlesConfig from "../assets/ParticlesConfig";
 function Home() {
-  return <></>;
+  return (
+    <div style={{ overflow: "hidden" }}>
+      <Particles params={particlesConfig} />
+      <h1 className="floatTxt">Hello</h1>
+    </div>
+  );
 }
 
 export default Home;
